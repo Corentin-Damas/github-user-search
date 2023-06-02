0bb4a623b0432055844bde5940b52a26206bd368
Nazemrap <corentin.damas@gmail.com>
2023-06-02 18:18:05 +0200

    JS opti

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ const themeSelect = document.querySelector(".luminosity-text");
 const themeIcon = document.querySelector(".icon-header");
 const btnSearch = document.querySelector(".btn-search");
 
+const inputValue = document.querySelector(".form_input").value;
+
+const userName = document.querySelector(".user-name-short");
+const userId = document.querySelector(".user-id");
+const biography = document.querySelector(".user-biography");
+const joinTimer = document.querySelector(".user-join-time");
+
 const nbRepository = document.querySelector(".nb-repository");
 const nbFollower = document.querySelector(".nb-follower");
 const nbFollowing = document.querySelector(".nb-following");
+
+const localUser = document.querySelector(".social-city-name");
+const twitterUser = document.querySelector(".social-twitter-page");
+const websiteUser = document.querySelector(".website-user");
+const compagniUser = document.querySelector(".user-compagnie");
 
 const imgUser = document.querySelector(".user-picture");
 
@@ -31,27 +43,90 @@ toggleThemeBtn.addEventListener("click", () => {
 let exempleName = "Corentin-damas";
 
 btnSearch.addEventListener("click", () => {
-  console.log("click");
-  const getAdvice = async function () {
+  const inputValue = document.querySelector(".form_input").value;
+
+  console.log(inputValue);
+  const getUserInfo = async function () {
     try {
       const response = await fetch(
-        `https://api.github.com/users/${exempleName}`
+        `https://api.github.com/users/${inputValue}`
       );
       const jsonData = await response.json();
-      console.log(jsonData);
-      console.log(jsonData.avatar_url);
 
-      imgUser.style.backgroundImage = `url(${jsonData.avatar_url})`;
+      //   user not found
 
-      nbRepository.textContent = `${jsonData.public_repos}`;
-      nbFollower.textContent = `${jsonData.followers}`;
-      nbFollowing.textContent = `${jsonData.following}`;
+      if (jsonData.message == "Not Found") {
+        userName.textContent = `No "${inputValue}" on github`;
+        userId.textContent = "n/a";
 
-      //   adviceText.textContent = `" ${jsonData.slip.advice} "`;
-      //   adviceId.textContent = `${jsonData.slip.id}`;
+        nbRepository.textContent = 0;
+        nbFollower.textContent = 0;
+        nbFollowing.textContent = 0;
+        joinTimer.textContent = "Joined DD Mmm YYYY";
+
+        //   user found
+      } else {
+        jsonData.name != null
+          ? (userName.textContent = `${jsonData.name}`)
+          : (userName.textContent = `${jsonData.login}`);
+        userId.textContent = `${jsonData.login}`;
+
+        imgUser.style.backgroundImage = `url(${jsonData.avatar_url})`;
+        if (jsonData.bio == null) {
+          biography.textContent = "This dev has no bio.";
+        }
+        if (jsonData.bio != null) {
+          biography.textContent = `${jsonData.bio}`;
+        }
+
+        nbRepository.textContent = `${jsonData.public_repos}`;
+        nbFollower.textContent = `${jsonData.followers}`;
+        nbFollowing.textContent = `${jsonData.following}`;
+
+        const dateJoined = new Date(jsonData.created_at);
+        let months = [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ];
+        let date = dateJoined.getDate();
+        let month = dateJoined.getMonth();
+        month = months[month];
+
+        if (date < 10) {
+          date = "0" + date;
+        }
+
+        joinTimer.textContent = `Joined ${date} ${month} ${dateJoined.getFullYear()}`;
+
+        jsonData.location != null
+          ? (localUser.textContent = `${jsonData.location}`)
+          : (localUser.style.color = "grey");
+
+        jsonData.twitter_username != null
+          ? (twitterUser.textContent = `${jsonData.twitter_username}`)
+          : (twitterUser.style.color = "grey");
+
+        jsonData.blog != null
+          ? (websiteUser.href = `${jsonData.blog}`)
+          : (websiteUser.style.color = "grey");
+
+        jsonData.company != null
+          ? (compagniUser.textContent = `${jsonData.company}`)
+          : (compagniUser.style.color = "grey");
+      }
     } catch (err) {
       alert(err);
     }
   };
-  getAdvice();
+  getUserInfo();
 });
